29c3a3e63ef0900568be6cbd0c84d99f35aad481
pTSern <tsern.dev@gmail.com>
2026-07-30 18:20:13 +0700

    Revert "push"
    This reverts commit e40136d2088ba156e2e257e8cb46402e6d8e533b.

--- assets/_$plugins/pts_language.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace pTS {
-    export namespace languages {
-        const _$langs = ["en"] as const;
-        export type TLang = typeof _$langs[number];
-        export function Enum(key: TLang): Record<string, string>;
-        export const path: string;
-        export const ELang: Record<string, string>;
-    }
-}
